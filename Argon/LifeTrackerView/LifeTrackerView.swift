7e07d2746ad4e25de364cb692e0e94abdb50b944
Charles Fiedler <chase.fiedler@skyward.io>
2024-05-12 14:50:47 -0500

    feat: added the logic to support lifeDelta tracking and plus and minus life button actions

--- Argon/LifeTrackerView/LifeTrackerView.swift
+++ Argon/LifeTrackerView/LifeTrackerView.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct LifeTrackerView: View {
 
   @Binding var count: Int
+  @Binding var lifeDelta: Int
   var backgroundColor: Color = .blue
+
+  @State var deltaTimer: Timer? = nil
+  @State var holdTimer: Timer? = nil
+  @State var isLongPressing: Bool = false
 
   var body: some View {
     GeometryReader { geom in
       HStack(alignment: .center) {
         Spacer()
         Button(action: {
-          print("Minus Button Pressed")
+          if(isLongPressing){
+            //this tap was caused by the end of a longpress gesture, so stop our fastforwarding
+            isLongPressing.toggle()
+            holdTimer?.invalidate()
+          } else {
+            count = count - 1
+            handleDeltaCounter(-1)
+          }
         }, label: {
           Image(systemName: "minus")
             .resizable()
@@ -25,11 +37,33 @@ struct LifeTrackerView: View {
             .getContrastColor(backgroundColor: backgroundColor)
             .modifier(FitImageToLifeTracker(geom: geom))
         })
+        .simultaneousGesture(
+          LongPressGesture(
+            minimumDuration: 0.2
+          ).onEnded { _ in
+            self.isLongPressing = true
+            holdTimer = Timer.scheduledTimer(
+              withTimeInterval: 0.1,
+              repeats: true,
+              block: { _ in
+                count = count - 1
+                handleDeltaCounter(-1)
+              }
+            )
+          }
+        )
 
         TrackerView(count: $count, trackerType: .life, backgroundColor: backgroundColor)
 
         Button(action: {
-          print("Plus Button Pressed")
+          if(isLongPressing){
+            //this tap was caused by the end of a longpress gesture, so stop our fastforwarding
+            isLongPressing.toggle()
+            holdTimer?.invalidate()
+          } else {
+            count = count + 1
+            handleDeltaCounter(1)
+          }
         }, label: {
           Image(systemName: "plus")
             .resizable()
@@ -37,18 +71,46 @@ struct LifeTrackerView: View {
             .getContrastColor(backgroundColor: backgroundColor)
             .modifier(FitImageToLifeTracker(geom: geom))
         })
+        .simultaneousGesture(
+          LongPressGesture(
+            minimumDuration: 0.2
+          ).onEnded { _ in
+            self.isLongPressing = true
+            holdTimer = Timer.scheduledTimer(
+              withTimeInterval: 0.1,
+              repeats: true,
+              block: { _ in
+                count = count + 1
+                handleDeltaCounter(1)
+              }
+            )
+          }
+        )
         Spacer()
       }
     }
     .background(backgroundColor)
   }
+
+  func handleDeltaCounter(_ value: Int) {
+    deltaTimer?.invalidate()
+    deltaTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true, block: { _ in
+      if lifeDelta != 0 {
+//        let newDelta = History.Delta(lifeDelta)
+//        history.deltas.append(newDelta)
+      }
+      lifeDelta = 0
+    })
+    lifeDelta = lifeDelta + value
+  }
 }
 
 struct LifeTrackerView_Previews: PreviewProvider {
   @State static var count = 22
+  @State static var delta = 2
 
   static var previews: some View {
-    LifeTrackerView(count: $count)
+    LifeTrackerView(count: $count, lifeDelta: $delta)
   }
 }
 
--- Argon/PlayerView/PlayerView.swift
+++ Argon/PlayerView/PlayerView.swift
@@ -17,12 +17,13 @@ struct PlayerView: View {
   var body: some View {
     GeometryReader { geom in
       VStack(alignment: .center, spacing: 0) {
-        Text("-4")
+        Text("\(lifeDelta)")
           .getContrastColor(backgroundColor: backgroundColor)
           .font(.system(size: 500))
           .minimumScaleFactor(0.01)
           .frame(maxHeight: geom.size.height * 0.10)
-        LifeTrackerView(count: $count, backgroundColor: backgroundColor)
+          .opacity(lifeDelta != 0 ? 1.0 : 0.0)
+        LifeTrackerView(count: $count, lifeDelta: $lifeDelta, backgroundColor: backgroundColor)
           .frame(minHeight: geom.size.height * 0.4)
         PlayerSettingsView(backgroundColor: backgroundColor)
           .frame(maxHeight: geom.size.height * 0.25)
